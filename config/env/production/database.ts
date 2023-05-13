f1b40786fe7d63e7e5a2e6d79d0ef560cbfa9e54
foucauld <foucauldbeaumont@yahoo.fr>
2023-05-13 02:02:06 +0100

    ssl conection

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -13,7 +13,9 @@ module.exports = ({ env }) => {
         user,
         password,
       },
-      ssl: "require",
+      ssl: {
+        rejectUnauthorized: false,
+      },
       debug: false,
     },
   };
